729e941c973dd467b6f0d76801f8c92525be50b8
shridarchandan <108406430+shridarchandan@users.noreply.github.com>
2024-04-07 20:32:43 +0200

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -25,6 +25,7 @@ function list_users_with_read_access {
         echo "users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo $collaborators
      fi
+}
 
 echo "listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
